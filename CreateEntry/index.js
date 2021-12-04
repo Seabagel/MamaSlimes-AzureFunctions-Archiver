84cac4b20df1dc71286a828a3b357f751cde98dc
Seabagel <hsrt1367tim@gmail.com>
2021-12-04 13:29:36 -0800

    Add 200 and 400 response

--- CreateEntry/index.js
+++ CreateEntry/index.js
@@ -1,13 +1,26 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  try {
+    const rb = req.body;
 
+    if (rb.id && rb.username && rb.messageBody) {
+      context.res = {
+        status: 201 /* Defaults to 200 */,
+        body: {
+          id: rb.id,
+          username: rb.username,
+          messageBody: rb.messageBody,
+        },
+      };
+    } else {
+      context.res = {
+        status: 400 /* Defaults to 200 */,
+      };
+    }
+  } catch (error) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 500,
     };
-}
+  }
+};
