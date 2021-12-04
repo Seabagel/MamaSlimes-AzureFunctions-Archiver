167f46c41181407c0a0ed0218951d70ec40158c6
Seabagel <hsrt1367tim@gmail.com>
2021-12-04 13:34:58 -0800

    Added context logs

--- CreateEntry/index.js
+++ CreateEntry/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+  context.log("Creating Entry - start");
 
   // Try reading the body
   try {
@@ -7,17 +7,19 @@ module.exports = async function (context, req) {
 
     if (rb.id && rb.username && rb.messageBody) {
       context.res = {
-        status: 201, // Success POST Created
+        status: 201, // Success
         body: {
           id: rb.id,
           username: rb.username,
           messageBody: rb.messageBody,
         },
       };
+      context.log("Status: 201 Success");
     } else {
       context.res = {
         status: 400, // Bad Request
       };
+      context.log("Status: 400 Bad Request");
     }
 
     // Catch Errors
@@ -25,5 +27,8 @@ module.exports = async function (context, req) {
     context.res = {
       status: 500, // Internal Server Error
     };
+    context.log("Status: 500 Internal Server Error");
   }
+
+  context.log("Creating Entry - finished");
 };
